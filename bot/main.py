f945c83684dbc757ef2bc1f966c362e81a89afdb
yadubuilds <mail@yadusins.com>
2026-01-16 05:19:40 +0530

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -3,7 +3,6 @@ from core.config import Config
 from bot.panel import register_panel
 from bot.update import register_update
 
-
 app = Client(
     "control-bot",
     api_id=Config.API_ID,
@@ -12,5 +11,7 @@ app = Client(
 )
 
 register_panel(app)
+register_update(app)
+
 print("Control bot started")
 app.run()
